e327f675b70502337c45a160a60ff96ab399e82b
EduardoBougo <eduardobougo@gmail.com>
2025-04-07 15:02:34 -0300

    Create FIX01_EX03.c

---
+++ Linguagem C/Lista_de_exercicios_de_fixação_01/FIX01_EX03.c
@@ -0,0 +1,26 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 07/04/2025
+Enunciado: 
+    Faça um programa em Linguagem C que peça uma temperatura em graus célsius e apresente seu respectivo valor em fahrenheit. O cálculo de conversão é: F = (9 * C / 5) + 32.
+    Casos de teste:
+    a) Para a temperatura de 50o Celsius, a temperatura em Fahrenheit é 122;
+    b) Para a temperatura de 18o Celsius, a temperatura em Fahrenheit é 64,4.
+
+*/
+
+#include<stdio.h>
+
+int main() {
+    // Declaração de variaveis
+    int temperatura_celsius = 0,
+        temperatura_fahrenheit = 0;
+
+    // Entrada
+    
+
+    // Processamento
+    //Saida
+    return 0;
+}
