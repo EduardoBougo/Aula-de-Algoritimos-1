2279ed8a3c41268ddf9480e44cd8ab65ec485c6c
EduardoBougo <eduardobougo@gmail.com>
2025-04-07 15:02:27 -0300

    Create FIX01_EX01.c

---
+++ Linguagem C/Lista_de_exercicios_de_fixação_01/FIX01_EX01.c
@@ -0,0 +1,33 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 07/04/2025
+*/
+/*
+Enunciado:
+    Faça um programa em Linguagem C que peça a idade do usuário e, em seguida, digite a mensagem: “Legal! Você tem *idade* anos!”.
+    Casos de teste:
+    a) Para a idade 18, a mensagem seria: Legal! Você tem 18 anos!
+    b) Para a idade 43, a mensagem seria: Legal! Você tem 43 anos!
+*/
+
+#include<stdio.h>
+
+int main() {
+    // Declaração de variaveis
+    int idade;
+
+    // Entrada
+    printf("Digite sua idade em anos:");
+    scanf("%d", &idade);
+    
+    //Saida
+    printf("Legal! Você tem %d anos!", idade);
+    return 0;
+}
+
+
+
+
+
+
