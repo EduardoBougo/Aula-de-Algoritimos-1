ace940ac40d88013422064c5e102ff61d8a7821f
EduardoBougo <eduardobougo@gmail.com>
2025-04-08 15:08:23 -0300

    Update FIX01_EX06.c

--- Linguagem C/Lista_de_exercicios_de_fixação_01/FIX01_EX06.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_01/FIX01_EX06.c
@@ -5,30 +5,30 @@ Data..: 07/04/2025
 Enunciado: 
     Elabore um programa em Linguagem C para calcular: (a + b)3
     Casos de teste:
-    a) Para os nÃºmeros 2 e 3, o cubo da soma Ã© 125;
-    b) Para os nÃºmeros 7 e 5, o cubo da soma Ã© 1728.
+    a) Para os números 2 e 3, o cubo da soma é 125;
+    b) Para os números 7 e 5, o cubo da soma é 1728.
 */
 
 #include<stdio.h>
 
 int main() {
-    // DeclaraÃ§Ã£o de variaveis
+    // Declaração de variaveis
     float numero_1 = 0,
         numero_2 = 0,
         soma = 0,
         cubo_da_soma = 0;
         
     // Entrada
-    printf("Digite o primeiro numero: ");
+    printf("Digite o primeiro número: ");
     scanf("%f", &numero_1);
-    printf("Digite o segundo numero: ");
+    printf("Digite o segundo número: ");
     scanf("%f", &numero_2);
 
     // Processamento
     soma = numero_1 + numero_2;
     cubo_da_soma = soma * soma * soma;
 
-    //Saida
-    printf("Para os numeros %.1f e %.1f, o cubo da soma Ã© %.1f", numero_1, numero_2, cubo_da_soma);
+    // Saida
+    printf("Para os números %.1f e %.1f, o cubo da soma é %.1f", numero_1, numero_2, cubo_da_soma);
     return 0;
 }
