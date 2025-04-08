7ed30b8fb073b9d3be5dec038be10fe04c6320ba
EduardoBougo <eduardobougo@gmail.com>
2025-04-07 23:44:27 -0300

    Update FIX01_EX06.c

--- Linguagem C/Lista_de_exercicios_de_fixação_01/FIX01_EX06.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_01/FIX01_EX06.c
@@ -2,16 +2,33 @@
 Nome..: Eduardo Camargo Bougo
 Truma.: EC41F Algoritos 1
 Data..: 07/04/2025
-Enunciado:
-
+Enunciado: 
+    Elabore um programa em Linguagem C para calcular: (a + b)3
+    Casos de teste:
+    a) Para os números 2 e 3, o cubo da soma é 125;
+    b) Para os números 7 e 5, o cubo da soma é 1728.
 */
 
 #include<stdio.h>
 
 int main() {
     // Declaração de variaveis
+    float numero_1 = 0,
+        numero_2 = 0,
+        soma = 0,
+        cubo_da_soma = 0;
+        
     // Entrada
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &numero_1);
+    printf("Digite o segundo numero: ");
+    scanf("%f", &numero_2);
+
     // Processamento
+    soma = numero_1 + numero_2;
+    cubo_da_soma = soma * soma * soma;
+
     //Saida
+    printf("Para os numeros %.1f e %.1f, o cubo da soma é %.1f", numero_1, numero_2, cubo_da_soma);
     return 0;
 }
