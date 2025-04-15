b4efbd34c998899de576f4e2af1052c1d72310e0
EduardoBougo <eduardobougo@gmail.com>
2025-04-15 16:44:02 -0300

    aa

--- Linguagem C/Lista_de_exercicios_de_fixação_02_B/FIX03_EX09.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_02_B/FIX03_EX09.c
@@ -21,8 +21,11 @@ int main() {
     int menu = 0,
         base_triangulo = 0,
         altura_triangulo = 0,
+        area_triangulo = 0,
         raio_circuferencia = 0,
-        aresta_cubo = 0;
+        area_circuferencia = 0,
+        aresta_cubo = 0,
+        area_cubo;
 
     // Entrada
     printf("\n====================================\n");
@@ -31,32 +34,66 @@ int main() {
     printf("\n 2) Calcular a área de um círculo.");
     printf("\n 3) Calcular a área de um cubo.\n");
     printf("\n====================================\n");
-    printf("Selecione a opcao: ");
+    printf("\nSelecione a opcao: ");
     scanf("%d", &menu);
+    printf("\n------------------------------------\n");
     // Processamento
     if (menu == 1) {
         // Triangulo
+        // Entrada
+        printf("\nVamos calcular a area de um triangulo!\n");
+        printf("\nDigite o tamanho da base do triangulo: ");
+        scanf("%d", &base_triangulo);
+        printf("\nDigite o tamanho da altura do triangulo: ");
+        scanf("%d", &altura_triangulo);
 
+        // Processamento
+        area_triangulo = (base_triangulo * altura_triangulo) / 2;
+
+        // Saida
+        printf("\nO resultado da area do triangulo e de: %d", area_triangulo);
     }
     else {
         if (menu == 2) {
             // Circulo
+            // Entrada
+            printf("\nVamos calcular a area de um circulo!\n");
+            printf("\nDigite o tamanho do raio do circulo: ");
+            scanf("%d", &raio_circuferencia);
+
+            // Processamento
+            area_circuferencia = 3,14 * (raio_circuferencia * raio_circuferencia);
+
+            // Saida
+            printf("\nO resultado da area da circuferencia e de: %d", area_circuferencia);
 
         }
         else {
             if (menu == 3) {
                 // Cubo
+                // Entrada
+                printf("\nVamos calcular a area de um cubo!\n");
+                printf("\nDigite o tamanho da aresta do cubo: ");
+                scanf("%d", &aresta_cubo);
+
+                // Processamento
+                area_cubo = 6 * (aresta_cubo * aresta_cubo);
+
+                // Saida
+                printf("\nO resultado da area docubo e de: %d", area_cubo);
 
             }
             else {
-                printf("\n!ERRO!\n");
-                printf("Opcao inexistente.\n")
+                printf("\n------!ERRO!------");
+                printf("\nOpcao inexistente.");
+                printf("\n------!ERRO!------");
             }
         }
     }
 
     // Saida
-    printf("Fim do programa!");
+    printf("\n\n------------------------------------\n");
+    printf("\nFim do programa!\n");
 
     return 0;
 }
