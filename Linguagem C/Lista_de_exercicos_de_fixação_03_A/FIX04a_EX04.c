a1279c10d91b41c902839fc26fa0332775f8e97d
EduardoBougo <Eduardobougo@gmail.com>
2025-04-28 17:21:39 -0300

    a

--- Linguagem C/Lista_de_exercicos_de_fixação_03_A/FIX04a_EX04.c
+++ Linguagem C/Lista_de_exercicos_de_fixação_03_A/FIX04a_EX04.c
@@ -3,7 +3,7 @@ Nome..: Eduardo Camargo Bougo
 Truma.: EC41F Algoritos 1
 Data..: 00/04/2025
 Enunciado:
-
+    Faça um programa em Linguagem C que receba um número do usuário e apresente a fase "Seja bem vindo", tantas vezes quanto for esse número. Um frase por linha.
 */
 
 #include<stdio.h>
@@ -14,8 +14,15 @@ int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
     
     // Declaração de variaveis
+    int cont = 0,
+        num = 0;
+
     // Entrada
-    // Processamento
-    // Saida
+    printf("Digite um valor: ");
+    scanf("%d", &cont)
+    
+    // Processamento e Saida
+    while (cont)
+
     return 0;
 }
