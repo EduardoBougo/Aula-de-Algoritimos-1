a45f5f70b6dea8dd22d7715078066978747920d9
EduardoBougo <eduardobougo@gmail.com>
2025-04-29 16:36:37 -0300

    a

--- Linguagem C/Lista_de_exercicos_de_fixação_03_A/FIX04a_EX07.c
+++ Linguagem C/Lista_de_exercicos_de_fixação_03_A/FIX04a_EX07.c
@@ -1,9 +1,9 @@
 /*
 Nome..: Eduardo Camargo Bougo
 Truma.: EC41F Algoritos 1
-Data..: 00/04/2025
+Data..: 29/04/2025
 Enunciado:
-
+    Faça um programa em Linguagem C para calcular a média de notas obtida por um grupo de 8 alunos.
 */
 
 #include<stdio.h>
@@ -14,8 +14,25 @@ int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
     
     // Declaração de variaveis
-    // Entrada
-    // Processamento
+    int cont = 1;
+
+    float media = 0,
+        nota,
+        soma = 0;
+
+    while (cont <= 8) {
+        // Entrada
+        printf("Digite a nota do aluno %d: ", cont);
+        scanf("%f", &nota);
+
+        // Processamento
+        soma += nota;
+
+        cont++;
+    }
+    media = soma / (cont - 1);
     // Saida
+    printf("A média dos alunos foi de %.2f", media);
+
     return 0;
 }
