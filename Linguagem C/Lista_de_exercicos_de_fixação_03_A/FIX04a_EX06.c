9feaa2242ad7b0dd6635a61a2910a57f0a17e984
EduardoBougo <eduardobougo@gmail.com>
2025-04-29 16:00:12 -0300

    a

--- Linguagem C/Lista_de_exercicos_de_fixação_03_A/FIX04a_EX06.c
+++ Linguagem C/Lista_de_exercicos_de_fixação_03_A/FIX04a_EX06.c
@@ -1,9 +1,9 @@
 /*
 Nome..: Eduardo Camargo Bougo
 Truma.: EC41F Algoritos 1
-Data..: 00/04/2025
+Data..: 29/04/2025
 Enunciado:
-
+     Faça um programa em Linguagem C para apresentar a tabuada de um número escolhido pelo usuário (1 a 10).
 */
 
 #include<stdio.h>
@@ -14,8 +14,18 @@ int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
     
     // Declaração de variaveis
+    int cont = 0,
+        tab = 0;
+
     // Entrada
-    // Processamento
-    // Saida
+    printf("Escolha qual tabuada usar: ");
+    scanf("%d", &tab);
+
+    // Processamento e Saida
+    while (cont <= 10) {
+        printf("\n%d x %2d = %3d", tab, cont, (cont * tab));
+        cont ++;
+    }
+
     return 0;
 }
