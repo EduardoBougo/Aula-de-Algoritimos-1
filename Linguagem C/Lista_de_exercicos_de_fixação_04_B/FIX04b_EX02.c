e707567320a8660568ec49515782ed1dbb06f918
EduardoBougo <eduardobougo@gmail.com>
2025-05-02 12:10:19 -0300

    a

--- Linguagem C/Lista_de_exercicos_de_fixação_04_B/FIX04b_EX02.c
+++ Linguagem C/Lista_de_exercicos_de_fixação_04_B/FIX04b_EX02.c
@@ -3,7 +3,8 @@ Nome..: Eduardo Camargo Bougo
 Truma.: EC41F Algoritos 1
 Data..: 02/05/2025
 Enunciado:
-
+    Faça um programa em Linguagem C que apresente os valores de 10 a 1.
+    Usando Do-While
 */
 
 #include<stdio.h>
@@ -14,17 +15,16 @@ int main() {
     setlocale(LC_ALL, "Portuguese_Brazil");
     
     // Declaração de variaveis
-
-
-    // Entrada
-
+    int cont = 10;
 
     // Processamento
-
+    do {
+        printf(".%d.", cont);
+        cont--;
+    } while (cont > 0);
 
     // Saida
 
-    
     printf("\nFim do código\n");
     return 0;
 }
---
+++ Linguagem C/Lista_de_exercicos_de_fixação_04_B/FIX04b_EX03.c
@@ -0,0 +1,23 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 00/05/2025
+Enunciado:
+    
+    Usando Do-While
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    // Entrada
+    // Processamento
+    // Saida
+    printf("\nFim do código\n");
+    return 0;
+}
