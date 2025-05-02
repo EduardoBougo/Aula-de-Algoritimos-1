6a517ee64291b963ee98dab5c13d527cd525b7d9
EduardoBougo <eduardobougo@gmail.com>
2025-05-02 13:02:51 -0300

    a

---
+++ Linguagem C/Lista_de_exercicos_de_fixação_04_B/FIX04b_EX04.c
@@ -0,0 +1,30 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 02/05/2025
+Enunciado:
+    Faça um programa em Linguagem C que receba 5 números do usuário. A cada número lido imprima na tela se ele é ou não múltiplo de 3.
+    Usando Do-While
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    
+
+    // Entrada
+
+
+    // Processamento
+
+
+    // Saida
+
+    printf("\nFim do código\n");
+    return 0;
+}
