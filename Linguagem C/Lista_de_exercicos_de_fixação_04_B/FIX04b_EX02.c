276aaa5b13d18d2626ef1d264b7342a382163ed2
EduardoBougo <eduardobougo@gmail.com>
2025-05-02 12:07:37 -0300

    Update FIX04b_EX02.c

---
+++ Linguagem C/Lista_de_exercicos_de_fixação_04_B/FIX04b_EX02.c
@@ -0,0 +1,30 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 02/05/2025
+Enunciado:
+
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+
+
+    // Entrada
+
+
+    // Processamento
+
+
+    // Saida
+
+    
+    printf("\nFim do código\n");
+    return 0;
+}
