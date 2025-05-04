7a0ed0c79b0110c16eae5abdfd0a18f89de47d5a
EduardoBougo <eduardobougo@gmail.com>
2025-05-04 11:02:43 -0300

    Create FIX04c_EX01.c

---
+++ Linguagem C/Lista_de_exercicos_de_fixação_04_C/FIX04c_EX01.c
@@ -0,0 +1,23 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 04/05/2025
+Enunciado:
+
+    Usando for
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    // Entrada
+    // Processamento
+    // Saida
+    printf("\nFim do código\n");
+    return 0;
+}
