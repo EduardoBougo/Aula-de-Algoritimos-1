0fe1713de75f45acad9a6661c87459e0da99adf4
EduardoBougo <eduardobougo@gmail.com>
2025-05-13 16:45:41 -0300

    a

--- Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX04.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX04.c
@@ -8,15 +8,40 @@ Enunciado:
 
 #include<stdio.h>
 #include<locale.h>
+#define TAM 8
 
 int main() {
     //Acentuação
     setlocale(LC_ALL, "Portuguese_Brazil");
     
     // Declaração de variaveis
+        int num[TAM],
+        i = 0,
+        cont = 0,
+        soma = 0;
+
+    float media = 0;
+
     // Entrada
+    for (i = 0; i < TAM; i++) {
+        printf("Sálario do %d° funcionários: ", i+1);
+        scanf("%d", &num[i]);
+        soma += num[i];
+    }
+
     // Processamento
+    media = soma / TAM;
+        for (i = 0; i < TAM; i++) {
+            if(num[i] < media){
+                cont++;
+            }
+    }
+
     // Saida
+    printf("\n==========================================\n");
+    printf("A media salarial foi de %.2f. E %d funcionários estão a baixo da média. ", media, cont);
+    printf("\n==========================================\n");
+
     printf("\nFim do código\n");
     return 0;
 }
