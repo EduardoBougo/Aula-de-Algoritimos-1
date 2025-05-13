bc7ab4606a165cf1be1b62f7db7db4722c87db72
EduardoBougo <eduardobougo@gmail.com>
2025-05-13 15:46:46 -0300

    a

---
+++ Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX01.c
@@ -0,0 +1,25 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 13/05/2025
+Enunciado:
+    Faça um programa em Linguagem C que peça 6 números inteiros e, depois, apresente-os na tela na forma invertida ao que foi digitado.
+    Casos de teste (ignore os _ ):
+    a) Para a entrada: 1 _ 3 _ 2 _ 4 _ 5 _ 9, a saída será: 9 _ 5 _ 4 _ 2 _ 3 _ 1
+    b) Para a entrada: 70 _ 18 _ 7 _ 51 _ 12 _ 6, a saída será: 6 _ 12 _ 51 _ 7 _ 18 _ 70
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    // Entrada
+    // Processamento
+    // Saida
+    printf("\nFim do código\n");
+    return 0;
+}
---
+++ Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX03.c
@@ -0,0 +1,30 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 13/05/2025
+Enunciado:
+    Faça um programa em Linguagem C que peça 5 números inteiros ao usuário. Armazene esses números em um vetor. Depois, o programa deve apresentar o maior valor armazenado no vetor.
+    Casos de teste (ignore os _ ):
+    a) Para a entrada: 10 _ 50 _ 20 _ 40 _ 30, apresentar como saída:
+    
+        Maior: 50
+    
+    b) Para a entrada: 8 _ 15 _ 9 _ 16 _ 5, apresentar como saída:
+    
+        Maior: 16
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    // Entrada
+    // Processamento
+    // Saida
+    printf("\nFim do código\n");
+    return 0;
+}
