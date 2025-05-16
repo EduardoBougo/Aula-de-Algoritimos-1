5d46184bff595fa559d1716405d0ff9819904989
EduardoBougo <eduardobougo@gmail.com>
2025-05-15 23:34:13 -0300

    a

--- Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX08.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX08.c
@@ -8,15 +8,48 @@ Enunciado:
 
 #include<stdio.h>
 #include<locale.h>
+#include<stdlib.h>
+#include<time.h>
+#define TAM 8
 
 int main() {
     //Acentuação
     setlocale(LC_ALL, "Portuguese_Brazil");
     
     // Declaração de variaveis
-    // Entrada
-    // Processamento
+    int vetor_a[TAM],
+        vetor_b[TAM],
+        vetor_c[TAM],
+        i = 0;
+
+    srand(time(NULL));
+    // Entrada e Processamento
+        // Definir o Vetor A e B. E somando A + b criando C
+    for (i = 0; i < TAM; i++) {
+        vetor_a[i] = rand() % 100;
+        vetor_b[i] = rand() % 100;
+        vetor_c[i] = vetor_a[i] + vetor_b[i];
+    }
+
     // Saida
-    printf("\nFim do código\n");
+        //Mostrando o vetor A 
+    printf("\n\nVetor A:  ");
+    for (i = 0; i < TAM; i++) {
+        printf("%d ", vetor_a[i]);
+    }
+
+        //Mostrando o vetor B
+    printf("\n\nVetor B:  ");
+    for (i = 0; i < TAM; i++) {
+        printf("%d ", vetor_b[i]);
+    }
+    
+        //Mostrando o vetor C 
+    printf("\n\nVetor C:  ");
+    for (i = 0; i < TAM; i++) {
+        printf("%d ", vetor_c[i]);
+    }
+
+    printf("\n\nFim do código\n");
     return 0;
 }
