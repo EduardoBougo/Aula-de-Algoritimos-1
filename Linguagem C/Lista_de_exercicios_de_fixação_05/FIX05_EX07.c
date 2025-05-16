153e3aab8a0d843527f586891817d0f732d2cbe0
EduardoBougo <eduardobougo@gmail.com>
2025-05-15 23:17:23 -0300

    a

--- Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX07.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_05/FIX05_EX07.c
@@ -1,7 +1,7 @@
 /*
 Nome..: Eduardo Camargo Bougo
 Truma.: EC41F Algoritos 1
-Data..: 13/05/2025
+Data..: 15/05/2025
 Enunciado:
     Faça um programa em Linguagem C que receba (ou gere aleatoriamente) 25 valores. Após digitados, o programa deve ordenar os valores no vetor e apresentá-los na tela em ordem decrescente.
 */
