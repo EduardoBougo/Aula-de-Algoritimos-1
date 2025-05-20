de9ef683b0cf7e62b1aa02138821d0ea2c7740b0
EduardoBougo <eduardobougo@gmail.com>
2025-05-20 15:13:57 -0300

    a

---
+++ Linguagem C/Lista_de_exercicios_de_fixação_06/FIX06_EX01.c
@@ -0,0 +1,47 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 20/05/2025
+Enunciado:
+    Faça um programa em Linguagem C que crie uma matriz de 2x2, de números inteiros. Populacione-a usando laço PARA (FOR) e, por fim, apresente todos os valores na tela (com aparencia de matriz).
+*/
+
+#include<stdio.h>
+#include<locale.h>
+#define LIN 2
+#define COL 2
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    int num[LIN] [COL],
+        i = 0,
+        j = 0;
+
+    // Entrada
+    for (i = 0; i < LIN; i++)
+    {
+        for (j = 0; j < COL; j++)
+        {
+            printf("Valor [%d] [%d] : ", i, j);
+            scanf("%d", &num[i] [j]);
+        }
+    }
+    
+    // Saida
+    printf("\n..:: Apresentando valores ::..");
+    printf("\n-----------------------");
+    for (i = 0; i < LIN; i++)
+    {
+        printf("\n");
+        for (j = 0; j < COL; j++)
+        {
+            printf("| %4.d |", num[i] [j]);
+        }
+    }
+
+    printf("\nFim do código\n");
+    return 0;
+}
