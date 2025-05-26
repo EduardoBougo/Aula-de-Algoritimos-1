3b30f5e7d2590cbab5bd1c17eb4938c8b39f3aea
EduardoBougo <eduardobougo@gmail.com>
2025-05-26 17:21:40 -0300

    a

--- Linguagem C/teste.c
+++ Linguagem C/teste.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
-#define TAM 20
+#define TAM 4
 
 int main() {
     int i = 0,
         num[TAM] = {};
 
+    for (i = 0; i < TAM; i++) {
+        scanf("%d", &num[i]);
+    }
     for (i = 0; i < TAM; i++) {
         printf("%d\n", num[i]);
     }
