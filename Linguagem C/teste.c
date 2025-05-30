4a929cbdbbe49f6b87f08314ec070bdf3bde27dd
EduardoBougo <eduardobougo@gmail.com>
2025-05-29 22:51:00 -0300

    a

--- Linguagem C/teste.c
+++ Linguagem C/teste.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
-#define TAM 4
-
+#define TAM 12
 int main() {
-    char nome[232423];
+    char op = ' ';
+ 
+    int i = 0,
+        j = 0,
+        lin = 0;
+    
+    float M[TAM][TAM],
+        soma = 0;
+        
+    scanf("%d", &lin);
+    scanf("%c", &op);
+    
+    for(i = 0; i < TAM; i++){
+        for(j = 0; j < TAM; j++){
+            scanf("%f", &M[i][j]);
+        }
+    }
+    for(i = 0; i < TAM; i++){
+            soma += M[lin][i];
+    }
+    
+    if(op == 'S'){
+        printf("%.1f\n", soma);
+    }
+    else{
+        printf("%.1f\n", (soma / 12));
+    }
 
-    printf("Digite seu nome bb: ");
-    fgets(nome, 232423, stdin);
-
-    printf("Nossa! %s Que nome bonito gatinho\n", nome);
     return 0;
 }
