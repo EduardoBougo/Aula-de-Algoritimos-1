18a1caa480833d632e22054fa021cd5b44d13817
EduardoBougo <eduardobougo@gmail.com>
2025-06-03 17:26:53 -0300

    a

--- Linguagem C/Lista_de_exercicios_de_fixação_08/FIX08_EX04.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_08/FIX08_EX04.c
@@ -20,6 +20,12 @@ Enunciado:
 
 void menu(void);
 void option(void);
+int repeat(void);
+void addition(void);
+void subtraction(void);
+void multiplication(void);
+void division(void);
+
 
 int main() {
     //Acentuação
@@ -55,87 +61,101 @@ void menu(void){
 
 void option(void){
     int option;
+    
+    while(option != 5){
+        menu();
 
-    menu();
+        printf("Opção: ");
+        scanf("%d", &option);
 
-    printf("Opção: ");
-    scanf("%d", &option);
-
-    switch(option){
-        case 1:
-            addition();
-            break;
-        case 2:
-            subtraction();
-            break;
-        case 3:
-            multiplication();
-            break;
-        case 4:
-            division();
-            break;
-        case 5:
-
-            break;
-        default:
-            printf("Opção invalida\n");
+        switch(option){
+            case 1:
+                addition();
+                break;
+            case 2:
+                subtraction();
+                break;
+            case 3:
+                multiplication();
+                break;
+            case 4:
+                division();
+                break;
+            case 5:
+                break;
+            default:
+                printf("Opção invalida\n");
+        }
     }
+
 }
 
 void addition(void){
     float sum = 0,
           num = 0;
 
-    int rep = repeat();
+    int rep = repeat(),
+        i = 0;
 
     for(i = 0; i < rep; i++){
-        printf("Digite o %d° valor (0 para a execução): ", (i +));
+        printf("Digite o %d° valor: ", (i + 1));
         scanf("%f", &num);
         sum += num;
     }
 
-    printf("A soma é igual a >>> %.2f <<<", sum);
+    printf("A soma é igual a >>> %.2f <<<\n", sum);
 }
 
 void subtraction(void){
     float sub = 0,
           num = 0;
 
-    int rep = repeat();
+    int rep = repeat(),
+        i = 0;
 
     for(i = 0; i < rep; i++){
-        printf("Digite um valor (0 para a execução): ");
+        printf("Digite um valor: ");
         scanf("%f", &num);
         sub -= num;
     }
 
-    printf("A subtração é igual a >>> %.2f <<<", sub);
+    printf("A subtração é igual a >>> %.2f <<<\n", sub);
 }
 
 void multiplication(void){
     float mult = 0,
           num = 0;
 
+    int rep = repeat(),
+        i = 0;
+
     for(i = 0; i < rep; i++){
-        printf("Digite um valor (0 para a execução): ");
-        scanf("%f", &num);
-        mult = mult * num;
+        printf("Digite um valor: ");
+        if(num != 0){
+            scanf("%f", &num);
+            mult = mult * num;  
+        }
     }
 
-    printf("A multiplicação é igual a >>> %.2f <<<", mult);
+    printf("A multiplicação é igual a >>> %.2f <<<\n", mult);
 }
 
 void division(void){
     float div = 0,
           num = 0;
 
+    int rep = repeat(),
+        i = 0;
+
     for(i = 0; i < rep; i++){
-        printf("Digite um valor (0 para a execução): ");
-        scanf("%f", &num);
-        div = div / num;
+        printf("Digite um valor: ");
+        if(num != 0){
+            scanf("%f", &num);
+            div = div / num;            
+        }
     }
 
-    printf("A divisão é igual a >>> %.2f <<<", div);
+    printf("A divisão é igual a >>> %.2f <<<\n", div);
 }
 
 int repeat(){
