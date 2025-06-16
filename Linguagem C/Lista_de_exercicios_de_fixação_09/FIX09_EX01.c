c2876177857ac358dff73a0f8c6928c0571056e9
EduardoBougo <eduardobougo@gmail.com>
2025-06-16 14:44:27 -0300

    a

---
+++ Linguagem C/Lista_de_exercicios_de_fixação_09/FIX09_EX01.c
@@ -0,0 +1,22 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 16/06/2025
+Enunciado:
+    Faça um programa em Linguagem C que receba e apresente na tela uma data. Esta data é um registro (struct) com dia, mês e ano (ambos inteiros).
+*/
+
+#include<stdio.h>
+#include<locale.h>
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+    
+    // Declaração de variaveis
+    // Entrada
+    // Processamento
+    // Saida
+    printf("\nFim do código\n");
+    return 0;
+}
