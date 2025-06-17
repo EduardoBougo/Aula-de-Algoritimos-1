37dfcb8b1ad6dd2e5f7ab7713fe9b3f431c88720
EduardoBougo <eduardobougo@gmail.com>
2025-06-17 15:00:48 -0300

    Version-1.0-Release
    O código está rodando como esperado, utilizando switch case para mostrar o mês por extenso. Está funcional, pórem não está eficiente.

---
+++ Linguagem C/Lista_de_exercicios_de_fixação_09/FIX09_EX03.c
@@ -0,0 +1,105 @@
+/*
+Nome..: Eduardo Camargo Bougo
+Truma.: EC41F Algoritos 1
+Data..: 17/06/2025
+Enunciado:
+    Complementando o EX02, cria uma nova função que deve apresentar a data recebida por parâmetro, por extenso.
+    Exemplo de saída: 26 de Agosto de 2024
+*/
+
+#include<stdio.h>
+#include<locale.h>
+#include<string.h>
+
+struct Data{
+    int dia;
+    int mes;
+    int ano;
+} dt; // Declaração de variaveis
+
+void mostrarData(struct Data dt);
+void mostrarMesPorExtenso(struct Data dt, char mesExtenso[]);
+
+int main() {
+    //Acentuação
+    setlocale(LC_ALL, "Portuguese_Brazil");
+
+    // Entrada
+    printf("Digite o dia: ");
+    scanf("%d", &dt.dia);
+
+    printf("Digite o mês: ");
+    scanf("%d", &dt.mes);
+
+    printf("Digite o ano: ");
+    scanf("%d", &dt.ano);
+
+    // Processamento e Saida
+    mostrarData(dt);
+
+    printf("\nFim do código\n");
+    return 0;
+}
+
+void mostrarMesPorExtenso (struct Data dt, char mesExtenso[]){
+    switch (dt.mes)
+    {
+    case 1:
+        strcpy(mesExtenso, "Janeiro");
+        break;
+        
+    case 2:
+        strcpy(mesExtenso, "Fevereiro");
+        break;
+        
+    case 3:
+        strcpy(mesExtenso, "Março");
+        break;
+
+    case 4:
+        strcpy(mesExtenso, "Abril");
+        break;
+            
+    case 5:
+        strcpy(mesExtenso, "Maio");
+        break;
+        
+    case 6:
+        strcpy(mesExtenso, "Junho");
+        break;
+        
+    case 7:
+        strcpy(mesExtenso, "Julho");
+        break;
+        
+    case 8:
+        strcpy(mesExtenso, "Agosto");
+        break;
+        
+    case 9:
+        strcpy(mesExtenso, "Setembro");
+        break;
+        
+    case 10:
+        strcpy(mesExtenso, "Outubro");
+        break;
+
+    case 11:
+        strcpy(mesExtenso, "Novembro");
+        break;
+        
+    case 12:
+        strcpy(mesExtenso, "Dezembro");
+        break;
+
+    default:
+        break;
+    }
+}
+
+void mostrarData (struct Data dt){
+    char mesExtenso[10];
+
+    mostrarMesPorExtenso(dt, mesExtenso);
+    printf("\nExemplo de saida: |%02d de %s de %04d|\n", dt.dia, mesExtenso, dt.ano);
+}
