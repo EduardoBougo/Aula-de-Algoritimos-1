fcc06ca584ef0e70a621ce3017670a3eab140a88
EduardoBougo <eduardobougo@gmail.com>
2025-06-18 23:12:42 -0300

    feat: Função diferençaEmDias adicionada
    Nova função que calcula e retorna o numero de dias de uma data. Ainda falta adicionar a função final para calcular a difefença entre as duas datas.

--- Linguagem C/Lista_de_exercicios_de_fixação_09/FIX09_EX06.c
+++ Linguagem C/Lista_de_exercicios_de_fixação_09/FIX09_EX06.c
@@ -10,37 +10,52 @@ Enunciado:
 #include<locale.h>
 #include<string.h>
 
+#define N_DATAS 2
+
 struct Data{
     int dia;
     int mes;
     int ano;
-} dt_1, dt_2; // Declaração de variaveis
+} dt[N_DATAS]; // Declaração do vetor com os dados
 
 void linha(void); // Mostrar uma linha, para separar conteudos
 void mostrarData(struct Data dt); // Mostra a Data no formato dd/mm/aaaa
 void mostrarDataPorExtenso(struct Data dt); // Mostra a Data no formato 17 de Junho de 2025
-void receberData(struct Data dt); // Recebe os dados da Data
+void receberData(struct Data *dt); // Recebe os dados da Data
 int validarData(struct Data dt); // Validar se a data é valida -- Dia(1 a 28, 29, 30 ou 31) -- Mês(1 a 12) -- Ano(1900 a 2025)
-int anoBissexto(struct Data dt);
+int anoBissexto(int ano); // Verifica se o ano é Bissexto -- 1 Verdadeiro -- 0 Falso
 int numDiasEmData(struct Data dt);
+int diferencaEmDias(struct Data dt_1, struct Data dt_2);
 
 
-    int diasEmCadaMes[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int diasEmCadaMesPadrao[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 
 int main() {
     //Acentuação
     setlocale(LC_ALL, "Portuguese_Brazil");
 
+    int i = 0;
+
     // Recebendo os valores de Data
-    receberData(dt_1); 
-    receberData(dt_2);
+    for(i = 0; i < N_DATAS; i++){
+        receberData(&dt[i]);
+    }
 
     // Processamento e Saida
-    printf("%d ", numDiasEmData(dt_1));
+    printf("\n%d ", numDiasEmData(dt[0]));
+    printf("\n%d ", numDiasEmData(dt[1]));
 
     printf("\nFim do código\n");
     return 0;
+}
+
+void linha (void){
+    printf("\n===================================\n");
+}
+
+void mostrarData (struct Data dt){
+    printf("\nExemplo de saida: |%02d/%02d/%04d|\n", dt.dia, dt.mes, dt.ano);
 }
 
 void mostrarDataPorExtenso (struct Data dt){
@@ -61,51 +76,47 @@ void mostrarDataPorExtenso (struct Data dt){
     printf("\nExemplo de saida: |%02d de %s de %04d|\n", dt.dia, meses[dt.mes - 1], dt.ano);
 }
 
-void mostrarData (struct Data dt){
-    printf("\nExemplo de saida: |%02d/%02d/%04d|\n", dt.dia, dt.mes, dt.ano);
-}
 
-void receberData (struct Data dt){
+void receberData (struct Data *dt){
     do
     {
         linha();
         printf("Digite o dia: ");
-        scanf("%d", &dt.dia);
+        scanf("%d", &dt->dia);
 
         printf("Digite o mês: ");
-        scanf("%d", &dt.mes);
+        scanf("%d", &dt->mes);
 
         printf("Digite o ano: ");
-        scanf("%d", &dt.ano);
+        scanf("%d", &dt->ano);
         linha();
-    } while (validarData(dt));
-}
-
-int anoBissexto (struct Data dt){ // Testando anos bissextos
-
-    if((dt.ano % 4) != 0){ // Não é divisivel por 4 -- portanto não é Bissexto
-        return 0;
-    }
-    else{ // É divisivel por 4 
-        if((dt.ano % 100) == 0){ // É divisivel por 100
-            if((dt.ano % 400) != 0){ // É divisivel por 100 e não é por 400 -- Portanto não é Bissexto
-                return 0;
-            }
-            else{ // É divisivel por 100 e por 400 -- Portanto é Bissexto
-                return 1;
-            }
-            return 0;
-        }
-        else{ // Não é divisivel por 100 -- Portanto é Bissexto
-            return 1;
-        }
-    }
+    } while ( !validarData(*dt) );
 }
 
 int validarData (struct Data dt){
 
+    int diasNoMes = diasEmCadaMesPadrao[dt.mes - 1]; // Atribui o número de dias no mês digitado
+    
+    if((dt.mes == 2) && anoBissexto(dt.ano)){ // Se for Fevereiro e for ano Bissexto, atribui 29 ao número de dias no mes
+        diasNoMes = 29;
+    }
 
-    if (anoBissexto(dt) && (dt.mes == 2)){ // Se o ano for Bissexto e for Fevereiro
+    if((dt.dia < 1) || (dt.dia > diasNoMes)){ // Testando se o dia é Valido
+        return 0;
+    }
+
+    if((dt.mes < 1) || (dt.mes > 12)){ // Testando se o mês é Valido
+        return 0;
+    }
+
+    if((dt.ano < 1900) || (dt.ano > 2025)){ // Testando se o ano é Valido
+        return 0;
+    }
+
+    return 1; // Retornar verdadeiro(1) pois a data é Valida
+
+    /*
+    if (anoBissexto(dt.ano) && (dt.mes == 2)){ // Se o ano for Bissexto e for Fevereiro
         diasEmCadaMes[1] = 29; // O número de dias será de 29 dias
     }
 
@@ -119,24 +130,36 @@ int validarData (struct Data dt){
         return 1;
     }
     return 0; // Retornar verdadeiro(1) pois a data é Valida
+    */
 }
 
-void linha (void){
-    printf("\n===================================\n");
+int anoBissexto (int ano){ // Testando anos bissextos
+    /* Testa se é divisivel por 4, e ao mesmo tempo, se é divisivel por 400 ou Não é divisivel por 100 */
+    /* Retorna -- (1) Verdadeiro -- (0) Falso */
+    return (ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0));
 }
 
 int numDiasEmData(struct Data dt){
-    int qtdDias = 0;
-    int i = 0;
+    int totalDias = 0;
 
-    qtdDias += ((dt.ano - 1900) * 365); // Somando os dias dos anos
-
-    for(i = dt.mes; i > 0; i--){
-        qtdDias += diasEmCadaMes[i]; // Somando os dias dos meses
+    /* Soma os dias dos anos anteriores */
+    for(int ano = 1900; ano < dt.ano; ano++){
+        totalDias += (anoBissexto(ano) ? 366 : 365); // Se o ano for Bissexto soma 366, se não soma 365
     }
-    
-    qtdDias += dt.dia;
 
-    return qtdDias;
+    /* Soma os dias dos mêses anteriores */
+    for(int mes = 1; mes < dt.mes; mes++){
+        if((mes == 2) && (anoBissexto(dt.ano))){
+            totalDias += 29;
+        }
+        else{
+            totalDias += diasEmCadaMesPadrao[mes - 1];
+        }
+    }
+
+    /* Soma os dias do mês atual */
+    totalDias += dt.dia;
+
+    return totalDias;
 }
 
